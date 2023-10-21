a7678e3664d29555fd97b728abb67e1ff300611c
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-10-22 00:31:12 +0100

    Add text to form

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -70,136 +70,139 @@ export default function ProfileForm() {
   }
 
   return (
-    <div className="p-10">
-      <hr style={{ color: "", backgroundColor: "grey", height: 1}}/>
-      <div className="flex justify-center h-full py-10 overflow-scroll">
-        <div className="flex-1 pr-10">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {/* story title */}
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Story Title"
-                        {...field}
-                        onChange={field.onChange}
-                        className="text-black outline-"
-                      />
-                    </FormControl>
-                
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+    <div className="p-10 h-full overflow-hidden">
+  <hr style={{ color: "", backgroundColor: "grey", height: 1 }} />
+  <div className="flex justify-center h-full py-10 overflow-x-hidden overflow-y-auto ">
+    <div className="flex-1 pr-10">
+      <h1 className="text-2xl font-semibold mb-4">Submit Your Story</h1>
+      <p className="text-gray-600 mb-8">
+        Share your important story with the world and contribute to our mission of upholding honesty and integrity.
+      </p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Story Title */}
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Story Title"
+                    {...field}
+                    onChange={field.onChange}
+                    className="text-black outline-"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-              {/* preview descipriton */}
-              <FormField
-                control={form.control}
-                name="preview"
-                render={({ field }) => (
-                  <FormItem>
-                    
-                    <FormLabel>Preview</FormLabel>
-                    <FormControl>
-                      <BigInput
-                        placeholder="Story preview"
-                        {...field}
-                        onChange={field.onChange}
-                        className="text-black"
-                      />
-                    </FormControl>
-               
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          {/* Story Preview */}
+          <FormField
+            control={form.control}
+            name="preview"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preview</FormLabel>
+                <FormControl>
+                  <BigInput
+                    placeholder="Story Preview"
+                    {...field}
+                    onChange={field.onChange}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-              {/* price */}
-              <FormField
-                control={form.control}
-                name="price"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Price</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Price"
-                        type="number"
-                        step="0.01"
-                        {...field}
-                        onChange={field.onChange}
-                        className="text-black"
-                      />
-                    </FormControl>
-                
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          {/* Price */}
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Price"
+                    type="number"
+                    step="0.01"
+                    {...field}
+                    onChange={field.onChange}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-              {/* form description */}
-              <FormField
-                control={form.control}
-                name="evidence"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Upload evidence</FormLabel>
-                    <FormControl>
-                      <div className="grid w-full max-w-sm items-center gap-1.5">
-                        <Input id="evidence" type="file" />
-                      </div>
-                    </FormControl>
-                    <FormDescription>
-          
-                    </FormDescription>
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-        </div>
-
-        <div className="flex-1">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {/* story descipriton */}
-              <FormField
-                control={form.control}
-                name="detailed"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Descirption</FormLabel>
-                    <FormControl>
-                      <BiggerInput
-                        placeholder="Your story in more detail"
-                        {...field}
-                        onChange={field.onChange}
-                        className="text-black"
-                      />
-                    </FormControl>
-                    <FormDescription></FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-            
-                
-              <div className="flex justify-end">
-                <Button type="submit">Submit</Button>
-              </div>
-            </form>
-          </Form>
-          
-        </div>
-        
-      </div>
-      <hr style={{ color: "", backgroundColor: "rgba(128, 128, 128, 0.2)", height: 2}}/>
+          {/* Evidence Upload */}
+          <FormField
+            control={form.control}
+            name="evidence"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Upload Evidence</FormLabel>
+                <FormControl>
+                  <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Input id="evidence" type="file" />
+                  </div>
+                </FormControl>
+                <FormDescription>
+                  Attach any supporting evidence (optional).
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
+
+    <div className="flex-1">
+      <h2 className="text-xl font-semibold mb-4">Story Details</h2>
+      <p className="text-gray-600 mb-8">
+        Provide a comprehensive description of your story. Your voice matters in our fight against misinformation.
+      </p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Story Description */}
+          <FormField
+            control={form.control}
+            name="detailed"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <BiggerInput
+                    placeholder="Your story in more detail"
+                    {...field}
+                    onChange={field.onChange}
+                    className="text-black"
+                  />
+                </FormControl>
+                <FormDescription>
+                  Provide a detailed description of your story.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end">
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  </div>
+  
+</div>
+
   );
 }
