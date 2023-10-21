fcedcd394d050dee4d8c77cc1e2900c9d266835f
Rahman, Arraf (k21034926) <arrafrahman22@gmail.com>
2023-10-21 20:29:40 +0100

    updating

--- src/componentFunctions/heroSection.tsx
+++ src/componentFunctions/heroSection.tsx
@@ -5,14 +5,15 @@ import { AspectRatio } from "@/components/ui/aspect-ratio"
 
 export function HeroSection() {
   return (
-    <div className="flex-row-reverse items-center justify-center text-center">
-        <h1 className="items-center justify-center font-bold text-7xl mt-20">
+    <div className="text-center flex  items-center justify-center">
+        <img src="/karimdrip.jpg" className="ml-20 mt-20 rounded-r-full z-30"></img>
+        <h1 className="items-center justify-center font-bold text-7xl -ml-60 mt-80 z-40">
             Giving a Voice to the Voice Less
         </h1>
-        <h2 className="text-5xl">
+        <h2 className="text-5xl -ml-96 mt-96 mr-40">
             Spill the tea 
         </h2>
-        <img src="/karimdrip.jpg" className="ml-20 -mt-20"></img>
+        
     </div>
   )
 }
