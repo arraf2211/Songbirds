58240259dcbd030e58dfad674668ec70d157ceb7
Chandru Suresh <sureshchandru2003@gmail.com>
2023-10-21 17:09:04 +0100

    feat(layout): changed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-full w-full'>
+      <body className={`${inter.className} h-full w-full`}>
+        <div className='h-full w-full bg-black text-white'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
