a44f3dc05e319fded2e745030065e3d6555fb878
Rahman, Arraf (k21034926) <arrafrahman22@gmail.com>
2023-10-21 20:35:26 +0100

    changing display

--- src/componentFunctions/heroSection.tsx
+++ src/componentFunctions/heroSection.tsx
@@ -7,12 +7,13 @@ export function HeroSection() {
   return (
     <div className="text-center flex  items-center justify-center">
         <img src="/karimdrip.jpg" className="ml-20 mt-20 rounded-r-full z-30"></img>
-        <h1 className="items-center justify-center font-bold text-7xl -ml-60 mt-80 z-40">
+        <h1 className="items-center justify-center font-bold text-7xl z-40">
             Giving a Voice to the Voice Less
-        </h1>
-        <h2 className="text-5xl -ml-96 mt-96 mr-40">
+            <h2 className="text-5xl ">
             Spill the tea 
-        </h2>
+            </h2>
+        </h1>
+        
         
     </div>
   )
