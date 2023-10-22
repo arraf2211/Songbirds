57e3299f651b2cfc813043bdf6dcf7ff8db8d14a
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-10-22 04:28:37 +0100

    Fix text in landing page

--- src/componentFunctions/cards.tsx
+++ src/componentFunctions/cards.tsx
@@ -1,63 +1,55 @@
-import * as React from "react"
+import * as React from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Button } from "@/components/ui/button"
-import { Parallax } from 'react-scroll-parallax';
-
-
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export function Cards() {
-    return (
+  return (
     <div className="space-x-5 flex flex-row items-center justify-center h-[600px]">
-        <Card className="w-[350px] h-[250px] ml-5 bg-white ">
-            <CardHeader>
-            <CardTitle> Trending Information </CardTitle>
-            <CardDescription>Discover the insights that matter most and gain a fresh perspective on what's happening around the world. </CardDescription>
-            </CardHeader>
-            <CardContent>
-            
-            </CardContent>
-            <CardFooter className="flex justify-center">
-            <Button className="">View</Button>
-            </CardFooter>
-        </Card>
+      <Card className="w-[350px] h-[310px] ml-5 bg-white">
+        <CardHeader>
+          <CardTitle>Discover Valuable Insights</CardTitle>
+          <CardDescription className="pt-4">
+          Uncover a world of valuable data, insights, and knowledge contributed by the community. Support data creators and access premium content through crowdfunding. Your journey to discovery begins here.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Discover Data</Button>
+        </CardFooter>
+      </Card>
 
-        <Card className="w-[350px] h-[250px]  ml-5">
-            <CardHeader className="h-[150px]">
-            <CardTitle>Speak your Truth</CardTitle>
-            <CardDescription>Create a new Post for others to view</CardDescription>
-            </CardHeader>
-            <CardContent>
-            
-            </CardContent>
-            <CardFooter className="flex justify-center">
-            <Button>Post</Button>
-            </CardFooter>
-        </Card>
+      <Card className="w-[350px] h-[310px] ml-5 bg-white">
+        <CardHeader>
+          <CardTitle>Share Your Stories</CardTitle>
+          <CardDescription className="pt-4">
+            Be a storyteller and share your knowledge, stories, and data with the world while preserving your anonymity through blockchain. Earn from crowdfunding as your stories become a valuable resource.
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Create Stories</Button>
+        </CardFooter>
+      </Card>
 
-        <Card className="w-[350px] h-[250px]  ml-5">
-            <CardHeader className="h-[150px]">
-            <CardTitle>View Something</CardTitle>
-            <CardDescription>Whether you're a data enthusiast, a curious mind, or a decision-maker, 
-                our trending data section is your window to real-time knowledge. </CardDescription>
-            </CardHeader>
-            <CardContent>
-            
-            </CardContent>
-            <CardFooter className="flex justify-center ">
-            <Button>Deploy</Button>
-            </CardFooter>
-        </Card>
-
+      <Card className="w-[350px] h-[310px] ml-5 bg-white">
+        <CardHeader>
+          <CardTitle>Build Your Reputation</CardTitle>
+          <CardDescription className="pt-4">
+          Build a strong reputation by consistently sharing accurate and valuable data. A higher reputation allows you to price your content higher, gain more rewards. Become a respected contributor!
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Grow Reputation</Button>
+        </CardFooter>
+      </Card>
     </div>
-
-    )
-  }
+  );
+}
